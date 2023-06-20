99e2e1aa474e112c9669c9b18d5fd78ec186cbdc
Liuba <kundasl1@gmail.com>
2023-06-21 02:09:48 +0300

    added screenshot

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 from cx_Freeze import setup, Executable
 
+buildOptions = dict(packages = [], excludes = [])
+
+import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+
+executables = [Executable('example_1.py', base=base, targetName = 'chat.exe')]
 setup(name='chatexampy', #name задает полное имя пакета.
       version = "0.1",
       description = "example chat",
@@ -8,6 +14,8 @@ setup(name='chatexampy', #name задает полное имя пакета.
       author_email = "kundasl1@gmail.com",
       #url = "http://www.blog.pythonlibrary.org",
       packages=["src"],
-      executables = [Executable("src")] #правильно
+      options = dict(build_exe = buildOptions),
+      executables = executables,
+      #executables = [Executable("src")] #правильно
       )
 input("Press Enter")
